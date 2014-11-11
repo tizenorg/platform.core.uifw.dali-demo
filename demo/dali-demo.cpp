a924b135a603d9c2583934a1ffe3f1776c6a56f9
Paul Wisbey <p.wisbey@samsung.com>
2014-11-11 14:29:20 +0000

    Text rendering with Glyphy
    (Rebased from Xiangyin)
    
    Change-Id: Ib516894b3ee594d94902dddc63270ccd182ddfd2

--- demo/dali-demo.cpp
+++ demo/dali-demo.cpp
@@ -67,6 +67,7 @@ int main(int argc, char **argv)
   demo.AddExample(Example("line-mesh.example", DALI_DEMO_STR_TITLE_LINE_MESH));
   demo.AddExample(Example("gradients.example", DALI_DEMO_STR_TITLE_COLOR_GRADIENT));
   demo.AddExample(Example("image-view.example", DALI_DEMO_STR_TITLE_IMAGE_VIEW));
+  demo.AddExample(Example("glyphy-text.example", "Glyphy Text"));
 
   demo.SortAlphabetically( true );
 
