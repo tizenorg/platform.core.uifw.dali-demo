8935e20c0a1fbfc858068f9e5ed1264ecb377ff2
Ferran Sole <ferran.sole@samsung.com>
2015-03-02 15:38:27 +0000

    Added path animation demo
    Change-Id: I4a723457a7cfaca87a20f771aa64f187ea160453

--- demo/dali-demo.cpp
+++ demo/dali-demo.cpp
@@ -42,6 +42,7 @@ int main(int argc, char **argv)
   demo.AddExample(Example("builder.example", "Script Based UI"));
   demo.AddExample(Example("image-scaling-irregular-grid.example", "Image Scaling Modes"));
   demo.AddExample(Example("text-view.example", "Text View"));
+  demo.AddExample(Example("path-animation.example", "Path Animation"));
   app.MainLoop();
 
   return 0;
