16acabb8203c09b29715a57aff522e47b8ff772c
Xiangyin Ma <x1.ma@samsung.com>
2015-03-19 18:52:45 +0000

    Glyphy SVG
    Change-Id: Id8a85228e5af8f575324a4a38a53415766be51d9

---
+++ examples/glyphy-svg/demo-common.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2012 Google, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Google Author(s): Behdad Esfahbod, Maysum Panju
+ */
+
+#ifndef DEMO_COMMON_H
+#define DEMO_COMMON_H
+
+#include "glyphy.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+
+#include <algorithm>
+#include <vector>
+
+#define STRINGIZE1(Src) #Src
+#define STRINGIZE(Src) STRINGIZE1(Src)
+
+#define ARRAY_LEN(Array) (sizeof (Array) / sizeof (*Array))
+
+#define TOLERANCE 0.35
+
+static inline void
+die (const char *msg)
+{
+  fprintf (stderr, "%s\n", msg);
+  exit (1);
+}
+
+template <typename T>
+T clamp (T v, T m, T M)
+{
+  return v < m ? m : v > M ? M : v;
+}
+
+
+#if defined(_MSC_VER)
+#define DEMO_FUNC __FUNCSIG__
+#else
+#define DEMO_FUNC __func__
+#endif
+
+struct auto_trace_t
+{
+  auto_trace_t (const char *func_) : func (func_)
+  { printf ("Enter: %s\n", func); }
+
+  ~auto_trace_t (void)
+  { printf ("Leave: %s\n", func); }
+
+  private:
+  const char * const func;
+};
+
+#define TRACE() auto_trace_t trace(DEMO_FUNC)
+
+#endif /* DEMO_COMMON_H */
