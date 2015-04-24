74e1ee6cca67237ec05588965e5f3c7126841788
Agnelo Vaz <agnelo.vaz@samsung.com>
2015-04-24 19:23:42 +0100

    TextField demo upgrade to folder style
    Change-Id: Ifb187801c668bc25a16a494ce830092623755816
    Signed-off-by: Agnelo Vaz <agnelo.vaz@samsung.com>

--- examples/text-field/text-field-example.cpp
+++ examples/text-field/text-field-example.cpp
@@ -37,10 +37,14 @@ const char* const BACKGROUND_IMAGE = DALI_IMAGE_DIR "button-up.9.png";
 
 const float BORDER_WIDTH = 4.0f;
 
+const Size ACTIVATE_BUTTON_SIZE( 60.0f, 60.0f );
+
 } // unnamed namespace
 
 /**
  * @brief The main class of the demo.
+ * Demo shows a Text field appearing in a container after the start button is pressed.
+ * Touching outside of the text field and it's container hides them until the button is pressed again.
  */
 class TextFieldExample : public ConnectionTracker
 {
@@ -58,6 +62,103 @@ public:
     // Nothing to do here.
   }
 
+  void SetUpLayer( Layer& layer, Size size )
+  {
+    layer = Layer::New();
+    layer.SetAnchorPoint(Dali::AnchorPoint::CENTER);
+    layer.SetParentOrigin(Dali::ParentOrigin::CENTER);
+    layer.SetSize( size );
+    layer.SetTouchConsumed( true ); // Touch Events will be blocked past this Layer.
+  }
+
+  void OnShrinkAnimationFinished( Animation&)
+  {
+    mLayer.Remove( mContainer );
+    mActivationButton.SetOpacity(1.0f);
+    mOutsideArea.SetOpacity(0.0f);
+  }
+
+  void OnTapOutsideArea( Actor actor, const TapGesture& gesture )
+  {
+    if ( actor == mOutsideArea )
+    {
+      Animation animation = Animation::New(.5f);
+      Vector3 halfSize(0.15f,0.15f,0.15f) ;
+      animation.AnimateTo(Property( mContainer, Actor::Property::SCALE ), halfSize, AlphaFunction::EASE_IN_OUT );
+      animation.Play();
+      animation.FinishedSignal().Connect( this, &TextFieldExample::OnShrinkAnimationFinished );
+      mTapDetector.Detach(mOutsideArea);
+      mTapDetector.DetectedSignal().Disconnect(this, &TextFieldExample::OnTapOutsideArea );
+    }
+  }
+
+  void SetUpOutsideTouchArea( ImageActor& imageActor, Actor& parent )
+  {
+    imageActor = Dali::Toolkit::CreateSolidColorActor(Color::BLUE);
+    imageActor.SetOpacity(0.0f);
+    imageActor.SetAnchorPoint( AnchorPoint::CENTER );
+    imageActor.SetParentOrigin( ParentOrigin::CENTER );
+    imageActor.SetResizePolicy( ResizePolicy::FILL_TO_PARENT, Dimension::ALL_DIMENSIONS );
+    imageActor.SetZ( -1.0f );
+    parent.Add( mOutsideArea );
+  }
+
+  void SetUpActivationButton( PushButton& button, Actor& parent )
+  {
+    button = PushButton::New();
+    button.SetButtonImage( ResourceImage::New( BACKGROUND_IMAGE ) );
+    button.SetParentOrigin( ParentOrigin::CENTER );
+    button.SetSize( ACTIVATE_BUTTON_SIZE );
+    parent.Add( button );
+    button.ClickedSignal().Connect( this, &TextFieldExample::OnActivateButtonClicked );
+  }
+
+  void SetUpContainer( Control& control, Size stageSize )
+  {
+    control = Control::New();
+    control.SetParentOrigin( ParentOrigin::CENTER );
+    control.SetSize( Vector2(stageSize.width*0.6f, stageSize.width*0.6f) );
+    control.SetBackgroundImage( ResourceImage::New( BACKGROUND_IMAGE ) );
+    control.SetScale( 0.3f );
+  }
+
+  void SetUpTextField( Control& field, Size stageSize, Actor& parent )
+  {
+    field = TextField::New();
+    field.SetAnchorPoint( AnchorPoint::TOP_LEFT );
+    field.SetResizePolicy( ResizePolicy::FIXED, Dimension::ALL_DIMENSIONS );
+    field.SetProperty( TextField::Property::TEXT, "Unnamed folder" );
+    field.SetProperty( TextField::Property::DECORATION_BOUNDING_BOX, Rect<int>( BORDER_WIDTH, BORDER_WIDTH, stageSize.width - BORDER_WIDTH*2, stageSize.height - BORDER_WIDTH*2 ) );
+    field.SetSize( Size(150.0f, 40.0f ));
+    field.SetBackgroundColor(Vector4( 0.0f, 0.0f, 0.0f, 0.25f));
+    field.SetParentOrigin( ParentOrigin::CENTER );
+    field.SetAnchorPoint( AnchorPoint::BOTTOM_CENTER );
+    parent.Add( field );
+  }
+
+  void EnlargeAnimation( Control& control, Animation& animation, Vector3 destinationPosition = Vector3::ZERO )
+  {
+    Vector3 fullSize(1.0f, 1.0f, 1.0f);
+
+    animation.AnimateTo(Property(control, Actor::Property::SCALE ), fullSize, AlphaFunction::EASE_IN_OUT );
+    animation.AnimateTo(Property(control, Actor::Property::POSITION ), destinationPosition, AlphaFunction::EASE_OUT );
+    animation.Play();
+  }
+
+  bool OnActivateButtonClicked( Button button )
+  {
+    button.SetOpacity(0.0f);
+    mLayer.Add( mContainer );
+    Animation animation = Animation::New(.9f);
+    EnlargeAnimation( mContainer, animation );
+    mOutsideArea.SetOpacity(0.2f);
+    mTapDetector = TapGestureDetector::New();
+    mTapDetector.Attach(mOutsideArea);
+    mTapDetector.Attach(mContainer);
+    mTapDetector.DetectedSignal().Connect(this, &TextFieldExample::OnTapOutsideArea);
+    return true;
+  }
+
   /**
    * One-time setup in response to Application InitSignal.
    */
@@ -68,30 +169,24 @@ public:
     Stage stage = Stage::GetCurrent();
 
     stage.KeyEventSignal().Connect(this, &TextFieldExample::OnKeyEvent);
+    Size stageSize = stage.GetSize();
 
-    Vector2 stageSize = stage.GetSize();
+    // Create layer for dimmed background image and to parent popup
+    SetUpLayer( mLayer, stageSize );
+    stage.Add( mLayer );
+    mLayer.RaiseToTop(); // Can be done on demand but this demo has nothing else to raise above
 
-    mContainer = Control::New();
-    mContainer.SetName( "Container" );
-    mContainer.SetParentOrigin( ParentOrigin::CENTER );
-    mContainer.SetSize( Vector2(stageSize.width*0.6f, stageSize.width*0.6f) );
-    mContainer.SetBackgroundImage( ResourceImage::New( BACKGROUND_IMAGE ) );
-    mContainer.GetChildAt(0).SetZ(-1.0f);
-    stage.Add( mContainer );
+    // touch area to close folder when touched
+    SetUpOutsideTouchArea( mOutsideArea, mLayer );
 
-    TextField field = TextField::New();
-    field.SetAnchorPoint( AnchorPoint::TOP_LEFT );
-    field.SetResizePolicy( ResizePolicy::FILL_TO_PARENT, Dimension::WIDTH );
-    field.SetResizePolicy( ResizePolicy::DIMENSION_DEPENDENCY, Dimension::HEIGHT );
+    // button to show text field
+    SetUpActivationButton( mActivationButton, mLayer );
 
-    mContainer.Add( field );
+    // container to hold text field
+    SetUpContainer( mContainer, stageSize );
 
-    field.SetProperty( TextField::Property::TEXT, "Hello" );
-    field.SetProperty( TextField::Property::DECORATION_BOUNDING_BOX, Rect<int>( BORDER_WIDTH, BORDER_WIDTH, stageSize.width - BORDER_WIDTH*2, stageSize.height - BORDER_WIDTH*2 ) );
-
-    Property::Value fieldText = field.GetProperty( TextField::Property::TEXT );
-
-    std::cout << "Displaying text: " << fieldText.Get< std::string >() << std::endl;
+    TextField textField;
+    SetUpTextField( textField, stageSize, mContainer );
   }
 
   /**
@@ -113,6 +208,10 @@ private:
   Application& mApplication;
 
   Control mContainer;
+  Layer mLayer;
+  ImageActor mOutsideArea;
+  PushButton mActivationButton;
+  TapGestureDetector mTapDetector;
 };
 
 void RunTest( Application& application )
