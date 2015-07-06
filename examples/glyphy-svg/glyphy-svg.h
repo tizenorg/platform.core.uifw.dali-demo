bb56c17d5179f10c4e82b0b6e249ed7bed03c4f2
root <x1.ma@samsung.com>
2015-07-06 16:38:16 +0100

    SVG rendering
    Change-Id: Icf1cb1132481d75f5edf48211d66cda7e38b4ce8

---
+++ examples/glyphy-svg/glyphy-svg.h
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* Intentionally doesn't have include guards */
+
+#include "glyphy.h"
+#include <iostream>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#undef NANOSVG_IMPLEMENTATION
+#include "nanosvg.h"
+
+#ifndef GLYPHY_SVG_PREFIX
+#define GLYPHY_SVG_PREFIX glyphy_svg_
+#endif
+
+#ifndef glyphy_svgNSVGimage
+#define glyphy_svg(name) GLYPHY_PASTE (GLYPHY_SVG_PREFIX, name)
+#endif
+
+namespace
+{
+const float epsilon = 0.1f;
+
+bool float_equal( float f1, float f2 )
+{
+  return fabsf(f1-f2) < epsilon;
+}
+
+}
+
+static glyphy_bool_t
+glyphy_svg(move_to) (float to_x, float to_y,
+              glyphy_arc_accumulator_t *acc)
+{
+  glyphy_point_t p1 = {static_cast<double>(to_x), static_cast<double>(to_y)};
+ // glyphy_arc_accumulator_close_path (acc);
+  glyphy_arc_accumulator_move_to (acc, &p1);
+  return glyphy_arc_accumulator_successful (acc);
+}
+
+static glyphy_bool_t
+glyphy_svg(line_to) (float to_x, float to_y,
+                     glyphy_arc_accumulator_t *acc)
+{
+  glyphy_point_t p1 = {static_cast<double>(to_x), static_cast<double>(to_y)};
+  glyphy_arc_accumulator_line_to (acc, &p1);
+  return glyphy_arc_accumulator_successful (acc);
+}
+
+static glyphy_bool_t
+glyphy_svg(cubic_to) (float control1_x, float control1_y,
+                      float control2_x, float control2_y,
+                      float to_x, float to_y,
+                      glyphy_arc_accumulator_t *acc)
+{
+  glyphy_point_t p1 = {static_cast<double>(control1_x), static_cast<double>(control1_y)};
+  glyphy_point_t p2 = {static_cast<double>(control2_x), static_cast<double>(control2_y)};
+  glyphy_point_t p3 = {static_cast<double>(to_x), static_cast<double>(to_y)};
+  glyphy_arc_accumulator_cubic_to (acc, &p1, &p2, &p3);
+  return glyphy_arc_accumulator_successful (acc);
+}
+
+static glyphy_bool_t
+glyphy_svg(segment_to) ( float from_x, float from_y,
+                         float control1_x, float control1_y,
+                         float control2_x, float control2_y,
+                         float to_x, float to_y,
+                         glyphy_arc_accumulator_t *acc)
+{
+  if( float_equal( control1_x, to_x )
+   && float_equal( control1_y, to_y )
+   && float_equal( control2_x, from_x )
+   && float_equal( control2_y, from_y ))
+  {
+    return glyphy_svg(line_to)(to_x, to_y, acc);
+  }
+  else
+  {
+    return glyphy_svg(cubic_to)( control1_x, control1_y, control2_x, control2_y, to_x, to_y, acc );
+  }
+}
+
+static glyphy_bool_t
+glyphy_svg(outline_decompose) (NSVGshape  *svgShape,
+                    glyphy_arc_accumulator_t *acc)
+{
+  NSVGpath* path;
+  int i = 0;
+  glyphy_bool_t success = true;
+  for (path = svgShape->paths; path != NULL; path = path->next)
+  {
+    float* p = &path->pts[0];
+    success = success && glyphy_svg(move_to)( p[0], p[1], acc );
+
+    for (i = 0; i < path->npts-1; i += 3)
+    {
+      float* p = &path->pts[i*2];
+      {
+        success = success && glyphy_svg(segment_to)( p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7], acc );
+      }
+    }
+
+    p = &path->pts[0];
+    //TODO: better handling for filling non-closed path
+    if( sqrt( ( p[path->npts*2-2]-p[0] )* ( p[path->npts*2-2]-p[0] ) + ( p[path->npts*2-1]-p[1] )*( p[path->npts*2-1]-p[1] )) > 2.0 && path->npts < 9)
+    {
+      // not a closed path
+      svgShape->fill.type = 0;
+    }
+  }
+  if( svgShape->fill.type > 0 )
+  {
+    glyphy_arc_accumulator_close_path (acc);
+  }
+
+  return success;
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ examples/glyphy-svg/svg-actor.cpp
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+//CLASS HEADER
+#include "svg-actor.h"
+
+// EXTERNAL HEADERS
+#include <sstream>
+
+// INTERNAL INCLUDES
+#include <dali/public-api/animation/constraints.h>
+#include <dali/public-api/common/dali-vector.h>
+#include <dali/public-api/object/property-map.h>
+
+#include <dali/devel-api/rendering/sampler.h>
+
+// LOCAL INCLUDES
+#define MAKE_STRING(A)#A
+#include "glyphy-common-glsl.h"
+#include "glyphy-sdf-glsl.h"
+#include "demo-fshader-glsl.h"
+#include "demo-vshader-glsl.h"
+
+Shader SVGActor::CreateShader()
+{
+  std::ostringstream fragmentShaderStringStream;
+  fragmentShaderStringStream<<"#extension GL_OES_standard_derivatives : enable\n"
+                            <<"precision highp float;\n"
+                            <<"precision highp int;\n"
+                            <<"#define GLYPHY_INFINITY 1e5\n"
+                            <<"#define GLYPHY_EPSILON  1e-5\n"
+                            <<"#define GLYPHY_MAX_D 0.75\n"
+                            <<glyphyCommonShaderSource
+                            <<glyphySdfShaderSource
+                            <<fragmentShaderSource;
+
+  std::string enableExtensionString("#extension GL_OES_standard_derivatives : enable\nprecision highp float;\nprecision highp int;\n");
+
+  Shader shader = Shader::New( vertexShaderSource,
+                               fragmentShaderStringStream.str() );
+
+  return shader;
+}
+
+void SVGActor::Initialize()
+{
+  mActor = Actor::New();
+  mGeometry = Geometry::New();
+  mMaterial = Material::New( CreateShader() );
+
+  mRenderer = Renderer::New( mGeometry, mMaterial );
+  mActor.AddRenderer( mRenderer );
+
+  // register uniforms
+  mIndexAtlasInfo = mActor.RegisterProperty( "uAtlasInfo", Vector4( 1024.f, 1024.f, 64.f, 8.f ) );
+  mIndexStrokeScale = mActor.RegisterProperty( "uStrokeScale", 1.f );
+  mIndexGradientCount = mActor.RegisterProperty( "uGradientCount", 0.f );
+  mIndexGradientTexturePos = mActor.RegisterProperty( "uGradientTexturePos", Vector2::ZERO );
+}
+
+void SVGActor::SetVertices( std::vector<Vertex>* vertexData )
+{
+  Property::Map vertexFormat;
+  vertexFormat["aPosition"] = Property::VECTOR2;
+  vertexFormat["aGlyphyInfo"] = Property::VECTOR2;
+  vertexFormat["aFillColor"] = Property::VECTOR4;
+  vertexFormat["aStrokeColot"] = Property::VECTOR4;
+  vertexFormat["aGradientParamX"] = Property::VECTOR3;
+  vertexFormat["aGradientParamY"] = Property::VECTOR3;
+  vertexFormat["aStrokeWidth"] = Property::FLOAT;
+  vertexFormat["aGradientIndex"] = Property::FLOAT;
+
+  unsigned int vertexCount = vertexData->size();
+  PropertyBuffer vertices = PropertyBuffer::New( vertexFormat, vertexCount );
+  vertices.SetData( &(vertexData->at(0)) );
+
+  unsigned int indexCount = vertexCount * 3u / 2u;
+  Vector<unsigned int> indexData;
+  indexData.Reserve( indexCount );
+  for( unsigned int i = 0; i < vertexCount; i+=4u )
+  {
+    indexData.PushBack( i );
+    indexData.PushBack( i+1 );
+    indexData.PushBack( i+2 );
+    indexData.PushBack( i+1 );
+    indexData.PushBack( i+2 );
+    indexData.PushBack( i+3 );
+  }
+
+  Property::Map indexFormat;
+  indexFormat["indices"] = Property::INTEGER;
+  PropertyBuffer indices = PropertyBuffer::New( indexFormat, indexCount  );
+  indices.SetData( &indexData[0] );
+
+  mGeometry.AddVertexBuffer( vertices );
+  mGeometry.SetIndexBuffer( indices );
+}
+
+void SVGActor::SetGlyphyAtlas( BufferImage glyphyAtlas )
+{
+  Sampler sampler = Sampler::New( glyphyAtlas, "sGlyphyAtlas" );
+  mMaterial.AddSampler( sampler );
+}
+
+void SVGActor::SetGradientTexturePos( const Vector2& gradientTexPos )
+{
+  mActor.SetProperty( mIndexGradientTexturePos, gradientTexPos );
+}
+
+void SVGActor::SetAtlasInfo( const Vector4& atlasInfo )
+{
+  mActor.SetProperty( mIndexAtlasInfo, atlasInfo );
+}
+
+void SVGActor::SetGradientCount( int count )
+{
+  mActor.SetProperty( mIndexGradientCount, static_cast<float>(count) );
+}
+
+void SVGActor::ConstraintStrokeScale( )
+{
+  Constraint strokeScaleConstraint = Constraint::New<float>(mActor, mIndexStrokeScale, Dali::EqualToConstraint());
+  strokeScaleConstraint.AddSource( LocalSource( Actor::Property::SCALE_X ) );
+  strokeScaleConstraint.Apply();
+}
+
+Actor SVGActor::GetMeshActor()
+{
+  return mActor;
+}
