ea0dc9f61a44202f238b62c9da22a70c96fda88a
Ferran Sole <ferran.sole@samsung.com>
2015-10-07 10:23:22 +0100

    Changes following "Remove Sampler scene object"
    Change-Id: I5c99b4c1b27d0f3b8fc8102620f5f8e6e90f399b

--- examples/hello-world/hello-world-example.cpp
+++ examples/hello-world/hello-world-example.cpp
@@ -16,9 +16,155 @@
  */
 
 #include <dali-toolkit/dali-toolkit.h>
+#include <dali/devel-api/rendering/renderer.h>
 
 using namespace Dali;
-using Dali::Toolkit::TextLabel;
+
+
+namespace
+{
+
+const char* IMAGE_PATH[] = {
+                            DALI_IMAGE_DIR "gallery-medium-1.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-2.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-3.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-4.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-5.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-6.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-7.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-8.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-9.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-10.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-11.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-12.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-13.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-14.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-15.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-16.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-17.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-18.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-19.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-20.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-21.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-22.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-23.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-24.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-25.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-26.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-27.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-28.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-29.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-30.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-31.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-32.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-33.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-34.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-35.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-36.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-37.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-38.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-39.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-40.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-41.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-42.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-43.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-44.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-45.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-46.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-47.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-48.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-49.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-50.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-51.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-52.jpg",
+                            DALI_IMAGE_DIR "gallery-medium-53.jpg",
+};
+
+struct VertexWithTexture
+{
+  Vector2 position;
+  Vector2 texCoord;
+};
+
+VertexWithTexture gQuadWithTexture[] = {
+                                        { Vector2( -0.5f, -0.5f ), Vector2( 0.0f, 0.0f ) },
+                                        { Vector2(  0.5f, -0.5f ), Vector2( 1.0f, 0.0f ) },
+                                        { Vector2( -0.5f,  0.5f ), Vector2( 0.0f, 1.0f ) },
+                                        { Vector2(  0.5f,  0.5f ), Vector2( 1.0f, 1.0f ) }
+};
+
+const char* VERTEX_SHADER_TEXTURE = DALI_COMPOSE_SHADER(
+    attribute mediump vec2 aPosition;\n
+    attribute mediump vec2 aTexCoord;\n
+    uniform mediump mat4 uMvpMatrix;\n
+    uniform mediump vec3 uSize;\n
+    varying mediump vec2 vTexCoord;\n
+    void main()\n
+    {\n
+      vec4 position = vec4(aPosition,0.0,1.0)*vec4(uSize,1.0);\n
+      gl_Position = uMvpMatrix * position;\n
+      vTexCoord = aTexCoord;\n
+    }\n
+);
+
+const char* FRAGMENT_SHADER_TEXTURE = DALI_COMPOSE_SHADER(
+    uniform lowp vec4 uColor;\n
+    uniform sampler2D sTexture;\n
+    varying mediump vec2 vTexCoord;\n
+
+    void main()\n
+    {\n
+      gl_FragColor = texture2D( sTexture, vTexCoord ) * uColor;\n
+    }\n
+);
+
+
+Geometry& QuadMesh()
+{
+  static Geometry mesh;
+  if( !mesh )
+  {
+    PropertyBuffer vertexBuffer;
+    Property::Map vertexFormat;
+    vertexFormat["aPosition"] = Property::VECTOR2;
+    vertexFormat["aTexCoord"] = Property::VECTOR2;
+
+    //Create a vertex buffer for vertex positions and texture coordinates
+    vertexBuffer = PropertyBuffer::New( vertexFormat, 4u );
+    vertexBuffer.SetData( gQuadWithTexture );
+
+    //Create the geometry
+    mesh = Geometry::New();
+    mesh.AddVertexBuffer( vertexBuffer );
+    mesh.SetGeometryType(Geometry::TRIANGLE_STRIP );
+  }
+  return mesh;
+}
+
+Renderer CreateRenderer( unsigned int index )
+{
+     //Create the renderer
+    Shader shader = Shader::New( VERTEX_SHADER_TEXTURE, FRAGMENT_SHADER_TEXTURE );
+    static Image image = ResourceImage::New(IMAGE_PATH[index]);
+    //Sampler textureSampler = Sampler::New( image, "sTexture" );
+    Material material = Material::New( shader );
+    material.AddTexture( image, "sTexture");
+    material.SetBlendMode( BlendingMode::OFF );
+    Renderer renderer = Renderer::New( QuadMesh(), material );
+
+
+    return renderer;
+}
+
+Actor CreateMeshActor( unsigned int index)
+{
+  Renderer renderer = CreateRenderer(index);
+  Actor meshActor = Actor::New();
+  meshActor.AddRenderer( renderer );
+  return meshActor;
+}
+}
+
 
 // This example shows how to create and display Hello World! using a simple TextActor
 //
@@ -45,10 +191,10 @@ public:
     Stage stage = Stage::GetCurrent();
     stage.SetBackgroundColor( Color::WHITE );
 
-    TextLabel textLabel = TextLabel::New( "Hello World" );
-    textLabel.SetAnchorPoint( AnchorPoint::TOP_LEFT );
-    textLabel.SetName( "hello-world-label" );
-    stage.Add( textLabel );
+    Actor actor = CreateMeshActor( 0 );
+    actor.SetPosition(100.0f,100.0f,0.0f);
+    actor.SetSize(100.0f,100.0f);
+    stage.Add( actor );
 
     // Respond to a click anywhere on the stage
     stage.GetRootLayer().TouchedSignal().Connect( this, &HelloWorldController::OnTouch );
