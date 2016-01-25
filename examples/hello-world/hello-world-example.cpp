73915d8904998a343f73eac2fb589fae6e202321
Xiangyin Ma <x1.ma@samsung.com>
2016-01-25 11:30:17 +0000

    Testing custom shaders with ImageView
    Change-Id: Icc07f22325dc491b5a5b138515215ac876e0d32e

--- examples/hello-world/hello-world-example.cpp
+++ examples/hello-world/hello-world-example.cpp
@@ -16,9 +16,44 @@
  */
 
 #include <dali-toolkit/dali-toolkit.h>
+#include <iostream>
 
 using namespace Dali;
 using Dali::Toolkit::TextLabel;
+
+namespace
+{
+
+const char* IMAGE_PATH( DEMO_IMAGE_DIR "gallery-medium-14.jpg" );
+
+const char* VERTEX_SHADER = DALI_COMPOSE_SHADER(
+  attribute mediump vec2 aPosition;\n
+  uniform mediump mat4 uMvpMatrix;\n
+  uniform mediump vec3 uSize;\n
+  varying mediump vec2 vTexCoord;\n
+  \n
+  void main()\n
+  {\n
+    mediump vec4 vertexPosition = vec4(aPosition, 0.0, 1.0);\n
+    vertexPosition.xyz *= uSize;\n
+    vertexPosition = uMvpMatrix * vertexPosition;\n
+    \n
+    vTexCoord = aPosition + vec2(0.5);\n
+    gl_Position = vertexPosition;\n
+  }\n
+);
+
+const char* FRAGMENT_SHADER = DALI_COMPOSE_SHADER(
+  varying mediump vec2 vTexCoord;\n
+  uniform sampler2D sTexture;\n
+  uniform lowp vec4 uColor;\n
+  \n
+  void main()\n
+  {\n
+    gl_FragColor = texture2D( sTexture, vTexCoord ) * uColor;\n
+  }\n
+);
+}
 
 // This example shows how to create and display Hello World! using a simple TextActor
 //
@@ -45,10 +80,50 @@ public:
     Stage stage = Stage::GetCurrent();
     stage.SetBackgroundColor( Color::WHITE );
 
-    TextLabel textLabel = TextLabel::New( "Hello World" );
-    textLabel.SetAnchorPoint( AnchorPoint::TOP_LEFT );
-    textLabel.SetName( "helloWorldLabel" );
-    stage.Add( textLabel );
+    Image image = ResourceImage::New(IMAGE_PATH);
+    Toolkit::ImageView imageView1 = Toolkit::ImageView::New( image);
+    Toolkit::ImageView imageView2 = Toolkit::ImageView::New( image);
+    Toolkit::ImageView imageView3 = Toolkit::ImageView::New( image);
+    Toolkit::ImageView imageView4 = Toolkit::ImageView::New( image);
+
+    imageView1.SetParentOrigin( ParentOrigin::TOP_LEFT );
+    imageView1.SetAnchorPoint( AnchorPoint::TOP_LEFT );
+    stage.Add( imageView1 );
+
+    imageView2.SetParentOrigin( ParentOrigin::TOP_RIGHT );
+    imageView2.SetAnchorPoint( AnchorPoint::TOP_RIGHT );
+    stage.Add( imageView2 );
+
+    imageView3.SetParentOrigin( ParentOrigin::BOTTOM_LEFT );
+    imageView3.SetAnchorPoint( AnchorPoint::BOTTOM_LEFT );
+    stage.Add( imageView3 );
+
+    imageView4.SetParentOrigin( ParentOrigin::BOTTOM_RIGHT );
+    imageView4.SetAnchorPoint( AnchorPoint::BOTTOM_RIGHT );
+    stage.Add( imageView4 );
+
+    std::cout<<"customVertexDefaultFragment"<<std::endl;
+    Property::Map customVertexDefaultFragment;
+    Property::Map customVertexShader;
+    customVertexShader[ "vertexShader" ] = VERTEX_SHADER;
+    customVertexDefaultFragment[ "shader" ] = customVertexShader;
+    imageView2.SetProperty( Toolkit::ImageView::Property::IMAGE, customVertexDefaultFragment );
+
+    std::cout<<"defaultVertexCustomFragment"<<std::endl;
+    Property::Map defaultVertexCustomFragment;
+    Property::Map customfragmentShader;
+    customfragmentShader[ "fragmentShader" ] = FRAGMENT_SHADER;
+    defaultVertexCustomFragment[ "shader" ] = customfragmentShader;
+    imageView3.SetProperty( Toolkit::ImageView::Property::IMAGE, defaultVertexCustomFragment );
+
+    std::cout<<"customVertexcustomFragment"<<std::endl;
+    Property::Map customVertexcustomFragment;
+    Property::Map customVertexFragmentShader;
+    customVertexFragmentShader[ "vertexShader" ] = VERTEX_SHADER;
+    customVertexFragmentShader[ "fragmentShader" ] = FRAGMENT_SHADER;
+    customVertexcustomFragment[ "shader" ] = customVertexFragmentShader;
+    imageView4.SetProperty( Toolkit::ImageView::Property::IMAGE, customVertexcustomFragment );
+
 
     // Respond to a click anywhere on the stage
     stage.GetRootLayer().TouchedSignal().Connect( this, &HelloWorldController::OnTouch );
