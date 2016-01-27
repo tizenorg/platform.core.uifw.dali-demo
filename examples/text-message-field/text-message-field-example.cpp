ffa43c66335a5f3451e21c20bbf2791ba301d9ae
Agnelo Vaz <agnelo.vaz@samsung.com>
2016-01-27 15:31:22 +0000

    TextLabel demo shows three labels - Used for AutoScroll
    Smaller than control
    RTL language
    Larger text than control
    
    Change-Id: I33d3556e5f8a35ebfa2e74ccb8425418a49bb9c4
    Signed-off-by: Agnelo Vaz <agnelo.vaz@samsung.com>

--- examples/text-message-field/text-message-field-example.cpp
+++ examples/text-message-field/text-message-field-example.cpp
@@ -30,9 +30,19 @@ namespace
 {
   const char* DESKTOP_IMAGE( DEMO_IMAGE_DIR "woodEffect.jpg" );
   const Vector2 DESKTOP_SIZE( Vector2( 1440, 1600 ) );
-  const Vector2 PHOTOBOX_SIZE( Vector2(330.0f, 80.0f ) );
+  const Vector2 BOX_SIZE( Vector2(330.0f, 80.0f ) );
+  const Vector2 SCROLLING_BOX_SIZE( Vector2(330.0f, 40.0f ) );
   const float MAX_OFFSCREEN_RENDERING_SIZE = 2048.f;
   const float SCREEN_BORDER = 5.0f; // Border around screen that Popups and handles will not exceed
+
+  enum Labels
+  {
+    SMALL = 1u << 0,
+    RTL = 1u << 1,
+    LARGE = 1u << 2,
+    RTL_LONG = 1u << 4,
+    NONE = 1u << 6,
+  };
 }
 /**
  * @brief The main class of the demo.
@@ -54,6 +64,45 @@ public:
   {
     // Nothing to do here.
   }
+
+
+  void CreateBox( const std::string& name, Actor& box, Actor parent, const Vector2& size )
+  {
+    box.SetName(name);
+    box.SetAnchorPoint( AnchorPoint::CENTER );
+    box.SetParentOrigin( ParentOrigin::CENTER );
+    box.SetResizePolicy( ResizePolicy::FIXED, Dimension::ALL_DIMENSIONS );
+    box.SetSize( size );
+    parent.Add( box );
+
+    Dali::Property::Map border;
+    border.Insert( "rendererType",  "border" );
+    border.Insert( "borderColor",  Color::WHITE );
+    border.Insert( "borderSize",  1.f );
+    box.SetProperty( Control::Property::BACKGROUND, border );
+  }
+
+  void CreateLabel( Actor& label, const std::string text, Actor parent, bool scrollOnStart, PushButton button )
+  {
+    label = TextLabel::New( text );
+    label.SetResizePolicy( ResizePolicy::FIXED, Dimension::ALL_DIMENSIONS );
+    label.SetResizePolicy( ResizePolicy::FILL_TO_PARENT, Dimension::ALL_DIMENSIONS );
+    label.SetPadding( Padding( 1.0f, 1.0f, 1.0f, 1.0f ) );
+    label.SetAnchorPoint( AnchorPoint::CENTER );
+    label.SetParentOrigin( ParentOrigin::CENTER );
+    parent.Add( label );
+
+    if ( scrollOnStart )
+    {
+      label.SetProperty(TextLabel::Property::ENABLE_AUTO_SCROLL, true);
+    }
+
+    button.SetResizePolicy( ResizePolicy::USE_NATURAL_SIZE, Dimension::ALL_DIMENSIONS );
+    button.SetParentOrigin( ParentOrigin::TOP_RIGHT );
+    button.SetAnchorPoint( AnchorPoint::TOP_LEFT );
+    parent.Add(button);
+  }
+
 
   /**
    * One-time setup in response to Application InitSignal.
@@ -85,22 +134,15 @@ public:
 
     rootActor.Add( desktop ); // Add desktop (content) to offscreen actor
 
-    // Create Photo Box A
-    Control photoBoxA = Control::New();
+    // Create Boxes
+    Control boxA = Control::New();
+    Control boxB = Control::New();
+    Control boxC = Control::New();
+    Control boxD = Control::New();
+    Control boxE = Control::New();
 
-    Dali::Property::Map border;
-    border.Insert( "rendererType",  "border" );
-    border.Insert( "borderColor",  Color::WHITE );
-    border.Insert( "borderSize",  1.f );
-    photoBoxA.SetProperty( Control::Property::BACKGROUND, border );
-
-    photoBoxA.SetName("photoBoxA");
-    photoBoxA.SetAnchorPoint( AnchorPoint::CENTER );
-    photoBoxA.SetParentOrigin( ParentOrigin::CENTER );
-    photoBoxA.SetResizePolicy( ResizePolicy::FIXED, Dimension::ALL_DIMENSIONS );
-    photoBoxA.SetSize( PHOTOBOX_SIZE );
-    photoBoxA.SetPosition( 0.0f, -500.0f, 1.0f );
-    desktop.Add( photoBoxA );
+    CreateBox( "boxA", boxA, desktop, BOX_SIZE );
+    boxA.SetPosition( 0.0f, -500.0f, 1.0f );
 
     // Create TextField
     TextField field = TextField::New();
@@ -108,13 +150,108 @@ public:
     field.SetResizePolicy( ResizePolicy::FILL_TO_PARENT, Dimension::ALL_DIMENSIONS );
     field.SetPadding( Padding( 1.0f, 1.0f, 1.0f, 1.0f ) );
     field.SetAnchorPoint( AnchorPoint::TOP_LEFT );
-    field.SetProperty( TextField::Property::TEXT, "Enter Title name"  );
+    field.SetProperty( TextField::Property::PLACEHOLDER_TEXT, "Enter Folder Name" );
     field.SetProperty( TextField::Property::DECORATION_BOUNDING_BOX, Rect<int>( SCREEN_BORDER, SCREEN_BORDER, mStageSize.width - SCREEN_BORDER*2, mStageSize.height - SCREEN_BORDER*2 ) );
-    photoBoxA.Add( field );
+    boxA.Add( field );
+
+    CreateBox( "boxB", boxB, desktop, SCROLLING_BOX_SIZE );
+    boxB.SetPosition( 0.0f, -400.0f, 1.0f );
+    Toolkit::PushButton scrollLargeButton = Toolkit::PushButton::New();
+    scrollLargeButton.ClickedSignal().Connect( this, &TextMessageFieldExample::OnButtonClickedLarge );
+    CreateLabel( mLargeLabel, "A Quick Brown Fox Jumps Over The Lazy Dog", boxB, false ,scrollLargeButton );
+
+
+    CreateBox( "boxC", boxC, desktop, SCROLLING_BOX_SIZE );
+    boxC.SetPosition( 0.0f, -300.0f, 1.0f );
+    Toolkit::PushButton scrollSmallButton = Toolkit::PushButton::New();
+    scrollSmallButton.ClickedSignal().Connect( this, &TextMessageFieldExample::OnButtonClickedSmall );
+    CreateLabel( mSmallLabel, "A Quick Brown Fox", boxC , true, scrollSmallButton );
+
+    CreateBox( "boxD", boxD, desktop, SCROLLING_BOX_SIZE );
+    boxD.SetPosition( 0.0f, -200.0f, 1.0f );
+    Toolkit::PushButton scrollRtlButton = Toolkit::PushButton::New();
+    scrollRtlButton.ClickedSignal().Connect( this, &TextMessageFieldExample::OnButtonClickedRtl );
+    CreateLabel( mRtlLabel, "مرحبا بالعالم", boxD , true, scrollRtlButton );
+
+    CreateBox( "boxE", boxE, desktop, SCROLLING_BOX_SIZE );
+    boxE.SetPosition( 0.0f, -100.0f, 1.0f );
+    Toolkit::PushButton scrollRtlLongButton = Toolkit::PushButton::New();
+    scrollRtlLongButton.ClickedSignal().Connect( this, &TextMessageFieldExample::OnButtonClickedRtlLong );
+    CreateLabel( mRtlLongLabel, " مرحبا بالعالم مرحبا بالعالم مرحبا بالعالم", boxE , false, scrollRtlLongButton );
+
 
     mPanGestureDetector = PanGestureDetector::New();
     mPanGestureDetector.DetectedSignal().Connect(this, &TextMessageFieldExample::OnPanGesture );
     mPanGestureDetector.Attach( desktop );
+  }
+
+  void EnableScrolling( Labels labels )
+    {
+      Actor label;
+      switch( labels )
+      {
+        case LARGE:
+        {
+          label = mLargeLabel;
+          break;
+        }
+        case RTL:
+        {
+          label = mRtlLabel;
+          break;
+        }
+        case SMALL:
+        {
+          label = mSmallLabel;
+          break;
+        }
+        case RTL_LONG:
+        {
+          label = mRtlLongLabel;
+          break;
+        }
+        case NONE:
+        {
+          return;
+        }
+      }
+
+      if ( labels != NONE )
+      {
+        Property::Value value = label.GetProperty( TextLabel::Property::ENABLE_AUTO_SCROLL);
+        if (value.Get< bool >())
+        {
+          label.SetProperty( TextLabel::Property::ENABLE_AUTO_SCROLL, false  );
+        }
+        else
+        {
+          label.SetProperty( TextLabel::Property::ENABLE_AUTO_SCROLL, true  );
+        }
+      }
+    }
+
+  bool OnButtonClickedSmall( Toolkit::Button button )
+  {
+    EnableScrolling( SMALL );
+    return true;
+  }
+
+  bool OnButtonClickedLarge( Toolkit::Button button )
+  {
+    EnableScrolling( LARGE );
+    return true;
+  }
+
+  bool OnButtonClickedRtl( Toolkit::Button button )
+  {
+    EnableScrolling( RTL );
+    return true;
+  }
+
+  bool OnButtonClickedRtlLong( Toolkit::Button button )
+  {
+    EnableScrolling( RTL_LONG );
+    return true;
   }
 
   /**
@@ -151,6 +288,11 @@ private:
   Vector2 mTargetActorPosition;
   Vector2 mTargetActorSize;
   Vector2 mStageSize;
+
+  TextLabel mLargeLabel;
+  TextLabel mSmallLabel;
+  TextLabel mRtlLabel;
+  TextLabel mRtlLongLabel;
 };
 
 void RunTest( Application& application )
